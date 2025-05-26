ee1d92afefe0bbc3492e02e1f08b49f5d7a196da
Thisara Jayamuni <thisara.jayamuni95@gmail.com>
2025-05-26 10:42:34 +1000

    login api

--- server.js
+++ server.js
@@ -13,16 +13,17 @@ app.use(cors());
 app.use(express.json());
 
 // Load routes
-const userRoutes = require("./routes/users.routes");
-const deviceRoutes = require("./routes/device.routes");
-const bbqRoutes = require("./routes/BBQ.routes");
-const faultRoutes = require("./routes/faultReport.routes");
+const userRoutes = require('./routes/users.routes');
+const deviceRoutes = require('./routes/device.routes');
+const bbqRoutes = require('./routes/BBQ.routes');
+const faultRoutes = require('./routes/faultReport.routes');
+const authRoutes = require('./routes/auth.routes');
 
-
-app.use("/api/users", userRoutes);
+app.use('/api/users', userRoutes);
 // app.use("/api/devices", deviceRoutes); todo: fix device routes
-app.use("/api/bbqs", bbqRoutes);
-app.use("/api/faults", faultRoutes);
+app.use('/api/bbqs', bbqRoutes);
+app.use('/api/faults', faultRoutes);
+app.use('/api/auth', authRoutes);
 
 // Connect to MongoDB
 mongoose
